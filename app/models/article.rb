0989c0e23c1107c645f2951455b8da2d78ea5ea7
Jon Holstead <jon.holstead1@gmail.com>
2016-07-06 18:20:19 +0000

    Create new template, action and route

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,9 +1,5 @@
 class Article < ActiveRecord::Base
   #Record validation
-  validates :title,
-            presence: true,
-            length: { minimum: 3, maximum: 50 }
-  validates :description,
-            presence: true,
-            length: { minimum: 10, maximum: 300 }
-end
+  validates :title, presence: true, length: { minimum: 3, maximum: 50 }
+  validates :description, presence: true, length: { minimum: 10, maximum: 300 }
+end 

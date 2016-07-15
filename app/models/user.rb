b78fd790a9fed4b83631be1bf315bad334cf333f
Jon Holstead <jon.holstead1@gmail.com>
2016-07-15 15:19:00 +0000

    Sign up users from UI

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,18 @@
 class User < ActiveRecord::Base
   has_many :articles
   
-  validates :username, presence: true, 
+  validates :username, presence: true,
                        uniqueness: { case_sensitive: false },
                        length: { minimum: 3, maximum: 25 }
   
   before_save { self.email = email.downcase }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email,    presence: true, 
+  validates :email,    presence: true,
                        uniqueness: { case_sensitive: false },
                        length: { maximum: 105 },
                        format: { with: VALID_EMAIL_REGEX }
                        
   has_secure_password
+  validates :password, presence: true,
+                       length: { minimum: 8, maximum: 105 }
 end
